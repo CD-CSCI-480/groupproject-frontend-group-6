c72e4759a722c4f120c42466ae1e4ddbfae75343
Aniaa-Johnson <77907859+aniaa-johnson@users.noreply.github.com>
2024-04-24 12:42:53 -0400

    Added image files via upload

---
+++ screens/CustomerSignUp.js
@@ -0,0 +1,215 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import ButtonWithBackground from '../components/ButtonWithBackground'
+import { useNavigation } from '@react-navigation/native';
+
+
+
+export default function Example(navigation) {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#e8ecf4' }}>
+      <View style={styles.container}>
+        <KeyboardAwareScrollView>
+          <View style={styles.header}>
+            <Image
+              alt="App Logo"
+              resizeMode="contain"
+              style={styles.headerImg}
+              source={{
+                uri: 'https://raw.githubusercontent.com/Aniaa-Johnson/FoodOrderingApp/main/assets/booking-app%20(1).png',
+              }} />
+
+            <Text style={styles.title}>
+              Sign Up for <Text style={{ color: '#075eec' }}>EZBOOKING</Text>
+            </Text>
+
+            <Text style={styles.subtitle}>
+            Effortlessly Manage Your Bookings !
+            </Text>
+  
+          </View>
+
+
+
+          <View style={styles.form}>
+             <Text style={styles.inputLabel}>Name</Text>
+              <TextInput
+              autoCapitalize="words" // Capitalize the first letter of each word
+              autoCorrect={false}
+              onChangeText={name => setForm({ ...form, name })}
+               placeholder="John Doe"
+               placeholderTextColor="#6b7280"
+               style={styles.inputControl}
+               value={form.name}
+               />
+               </View>
+
+          <View style={styles.form}>
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Email address</Text>
+
+              <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                onChangeText={email => setForm({ ...form, email })}
+                placeholder="john@example.com"
+                placeholderTextColor="#6b7280"
+                style={styles.inputControl}
+                value={form.email} />
+            </View>
+
+            <View style={styles.input}>
+              <Text style={styles.inputLabel}>Password</Text>
+
+              <TextInput
+                autoCorrect={false}
+                onChangeText={password => setForm({ ...form, password })}
+                placeholder="********"
+                placeholderTextColor="#6b7280"
+                style={styles.inputControl}
+                secureTextEntry={true}
+                value={form.password} />
+            </View>
+
+            <View style={styles.formAction}>
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}>
+                <View style={styles.btn}>
+                  <Text style={styles.btnText}>Sign Up</Text>
+
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+
+        <TouchableOpacity
+          onPress={() => {
+            // handle link
+      
+          }}
+          style={{ marginTop: 'auto' }}>
+          <Text style={styles.formFooter}>
+            Returning Customer?{' '}
+            <Text style={{ textDecorationLine: 'underline' }}>Sign In</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
+    paddingHorizontal: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  title: {
+    fontSize: 31,
+    fontWeight: '700',
+    color: '#1D2A32',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+  },
+  /** Header */
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 36,
+  },
+  headerImg: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginBottom: 38,
+  },
+  /** Form */
+  form: {
+    marginBottom: 27,
+    paddingHorizontal: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  formAction: {
+    marginTop: 4,
+    marginBottom: 10,
+    color:'#075eec'
+  },
+  formLink: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#075eec',
+    textAlign: 'center',
+  },
+  formFooter: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'center',
+    letterSpacing: 0.15,
+  },
+  /** Input */
+  input: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#222',
+    marginBottom: 8,
+  },
+  inputControl: {
+    height: 50,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#222',
+    borderWidth: 1,
+    borderColor: '#C9D3DB',
+    borderStyle: 'solid',
+  },
+  /** Button */
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#6495ed',
+    borderColor: '#6495ed',
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: 'black',
+  },
+});
